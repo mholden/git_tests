b0ede23632411024e9cb187f8c53b20c6f9f2551
Max Holden <max_holden@apple.com>
2015-09-17 18:34:24 -0700

    change

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ main(int argc, char **argv)
 {
 	printf("Hello world!\n");
 
-	printf("This is a new branch.\n");
+	printf("This is a new branch, and a master change.\n");
 
 	printf("This is a change to a new branch.\n");
 
